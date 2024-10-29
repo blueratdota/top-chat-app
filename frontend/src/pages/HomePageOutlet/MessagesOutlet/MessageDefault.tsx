344aff42f4c97a63cbb6110405ebeba9a6fd07eb
blueratdota <jrafaelgocong@gmail.com>
2024-10-29 23:11:01 +0800

    responsive fix 2

--- frontend/src/pages/HomePageOutlet/MessagesOutlet/MessageDefault.tsx
+++ frontend/src/pages/HomePageOutlet/MessagesOutlet/MessageDefault.tsx
@@ -1,4 +1,4 @@
 const MessageDefault = () => {
-  return <div>Display this div if no message is clicked</div>;
+  return <div>DISPLAY SOMETHING DEFAULT HERE</div>;
 };
 export default MessageDefault;
